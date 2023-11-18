e1ff1c98949195853931ce189feaf03b1fc119bf
digispace-dingbang <dingbang@digispace-tw.com>
2023-11-18 22:45:16 +0800

    Initial commit

---
+++ Assets/Plugins/SunShine Android Native Screen Recorder/Scripts/ExampleScreenRecorder.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExampleScreenRecorder : MonoBehaviour
+{
+	[SerializeField] private string folderName;
+	[SerializeField] private bool isAudioRecording = true;
+	//	[SerializeField] private bool isAutoPreviewEnabled = false;
+	[Header("a * 512 * 512 . Please change the value of a" )]
+	[SerializeField] private int bitrate;
+	[SerializeField] private int fps;
+	[Header("value should be 0, 90, 180, 270  degree")]
+	[SerializeField] private int videoRotation = 0;
+	[SerializeField] private SmileSoftScreenRecordController.VideoEncoder videoEncoder = SmileSoftScreenRecordController.VideoEncoder.H264;
+
+
+	[SerializeField] private GameObject afterVideoCompletePanel;
+	[SerializeField] private Text savedPathText;
+	[SerializeField] private Button previewButton;
+	[SerializeField] private Button ShareButton;
+	[SerializeField] private AlertPanel alertPanel;
+
+	private string _recordedFilePath;
+
+	void Start()
+	{
+		alertPanel.Hide();
+		HideAfterVideoCompletePanel();
+		SetUp();
+	}
+
+	void SetUp()
+	{
+		// If want to store video in persistant data Path (Private Path) then use following line
+		//SmileSoftScreenRecordController.instance.SetVideoStoringDestination(Application.persistentDataPath);
+		//Do not want to show stored videos in gallery ,then uncomment following line.
+		//SmileSoftScreenRecordController.instance.SetGalleryAddingCapabilities(false);
+
+		SmileSoftScreenRecordController.instance.SetStoredFolderName(folderName); // only Android
+		SmileSoftScreenRecordController.instance.SetBitRate(bitrate); // only Android
+		SmileSoftScreenRecordController.instance.SetFPS(fps); // only Android
+
+		SmileSoftScreenRecordController.instance.SetVideoRotation(videoRotation); // only Android 
+		SmileSoftScreenRecordController.instance.SetVideoEncoder((int)videoEncoder); // only Android
+		SmileSoftScreenRecordController.instance.SetVideoSize((int)(Screen.width), (int)(Screen.height)); // only Android
+
+		SmileSoftScreenRecordController.instance.SetAudioCapabilities(isAudioRecording);  // both Android & iOS
+	}
+
+
+	public void StartRecording()
+	{
+		SetFileName();
+
+		bool iSitIos = SmileSoftScreenRecordController.instance.IsIosPlatform();
+		// If it is iOS 
+		if (iSitIos && SmileSoftScreenRecordController.instance.IsRecordingAvailable() == false)
+		{
+			// show error message
+			alertPanel.ShowAlert("Recorder Unavailable in this device!");
+			return;
+        }
+
+		SmileSoftScreenRecordController.instance.StartRecording();
+
+	}
+	public void StopRecording()
+	{
+		_recordedFilePath = null;
+		_recordedFilePath = SmileSoftScreenRecordController.instance.StopRecording();
+
+		if (SmileSoftScreenRecordController.instance.IsIosPlatform())
+        {
+			PreviewVideo();
+        }
+		if (SmileSoftScreenRecordController.instance.IsAndroidPlatform())
+        {
+			ShowAndroidVideoCompletatoonDialog();
+		}
+			
+	}
+
+	private void SetFileName()
+	{
+		System.DateTime now = System.DateTime.Now;
+		string date = now.ToShortDateString().Replace('/', '_')
+					+ now.ToLongTimeString().Replace(':', '_');
+		string fileName = "Record_" + date;
+
+		SmileSoftScreenRecordController.instance.SetVideoName(fileName);
+	}
+
+	private void ShowAndroidVideoCompletatoonDialog()
+	{
+		afterVideoCompletePanel.SetActive(true);
+		if (_recordedFilePath != null && File.Exists(_recordedFilePath))
+		{
+			previewButton.interactable = true;
+			ShareButton.interactable = true;
+			savedPathText.text = "Video saved successfully at : " + _recordedFilePath;
+		}
+		else
+		{
+			previewButton.interactable = false;
+			ShareButton.interactable = false;
+			savedPathText.text = "Error occured. Can not record video";
+		}
+
+	}
+
+	public void  PreviewVideo()
+	{
+		//for iOS this perameter is not affect anything. You can just send a null or empty value as a perameter for iOS 
+	    SmileSoftScreenRecordController.instance.PreviewVideo(_recordedFilePath);
+	}
+
+	public void ShareVideo()
+	{
+		SmileSoftScreenRecordController.instance.ShareVideo(_recordedFilePath, "Greetings From SmileSoft", "Sunshine Native Share");
+	}
+
+	public void HideAfterVideoCompletePanel()
+	{
+		afterVideoCompletePanel.SetActive(false);
+	}
+
+}
---
+++ Assets/Plugins/SunShine Android Native Screen Recorder/Scripts/SmileSoftScreenRecordController.cs
@@ -0,0 +1,271 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class SmileSoftScreenRecordController : MonoBehaviour
+{
+	public static SmileSoftScreenRecordController instance;
+
+	private string _fileProvider = "com.SmileSoft.unityplugin.ScreenRecordProvider";
+
+	private AndroidJavaObject screenRecorder;
+
+	private ReplayKitHelper _iosRecorder;
+
+	private bool _isIosTryingToStartRecording = false;
+
+	private void Awake()
+	{
+		if (instance == null)
+			instance = this;
+
+		Setup();
+	}
+
+    private void Start()
+    {
+		//Debug.Log("U>> Height is " + Screen.height + " Width " + Screen.width);
+    }
+
+
+	void OnApplicationFocus(bool hasFocus)
+	{
+		if (IsIosPlatform() && hasFocus && _isIosTryingToStartRecording)
+		{
+			Invoke("CheckIsRecording", 1.0f);
+		}
+	}
+
+	private void CheckIsRecording()
+    {
+		OnIosRecordStatus(_iosRecorder.IsRecording());
+	}
+
+	void Setup()
+	{
+		if (IsAndroidPlatform())
+		{
+			screenRecorder = new AndroidJavaObject("com.SmileSoft.unityplugin.ScreenCapture.ScreenRecordFragment");
+			screenRecorder?.Call("SetUp");
+		}
+
+		if (IsIosPlatform())
+		{
+			 _iosRecorder = new ReplayKitHelper();
+		}
+	}
+
+	public void StartRecording()
+	{
+		if (IsAndroidPlatform())
+			screenRecorder?.Call("StartRecording");
+
+		if (IsIosPlatform())
+        {
+
+			if (_iosRecorder.IsRecorderAvailable())
+            {
+				_isIosTryingToStartRecording = true;
+				_iosRecorder.StartRecording();
+			}
+            else
+            {
+				OnIosRecordStatus(false);
+			}
+        }
+			
+	}
+
+	public string StopRecording()
+	{
+		if (IsAndroidPlatform())
+		{
+			string recordedPath = screenRecorder?.Call<string>("StopRecording");
+			Debug.Log("Unity>> record path : " + recordedPath);
+			return recordedPath;
+		}
+
+		if (IsIosPlatform())
+        {
+			_iosRecorder.StopRecording();
+        }
+			
+
+		return null;
+	}
+
+	
+
+	public void SetVideoStoringDestination(string destination)
+	{
+		if (IsAndroidPlatform())
+			screenRecorder?.Call("SetVideoStoringDestination", destination);
+	}
+	public void SetStoredFolderName(string folderName)
+	{
+		if (IsAndroidPlatform())
+			screenRecorder?.Call("SetVideoStoredFolderName", folderName);
+	}
+
+	public void SetVideoName(string videoName)
+	{
+		if (IsAndroidPlatform())
+			screenRecorder?.Call("SetVideoName", videoName);
+	}
+
+	public void SetGalleryAddingCapabilities(bool canAddintoGallery)
+	{
+		if (IsAndroidPlatform())
+			screenRecorder?.Call("SetGalleryAddingCapabilities", canAddintoGallery);
+	}
+
+	public void SetAudioCapabilities(bool canRecordAudio)
+	{
+		if (IsAndroidPlatform())
+			screenRecorder?.Call("SetAudioCapabilities", canRecordAudio);
+		if (IsIosPlatform())
+			_iosRecorder.SetAudioCapability(canRecordAudio);
+	}
+	public void SetFPS(int fps)
+	{
+		if (IsAndroidPlatform())
+			screenRecorder?.Call("SetVideoFps", fps);
+	}
+
+	public void SetVideoRotation(int rotation)
+	{
+		if (IsAndroidPlatform())
+			screenRecorder?.Call("SetVideoRotation", rotation);
+	}
+
+	public void SetBitRate(int bitRate)
+	{
+		if (IsAndroidPlatform())
+			screenRecorder?.Call("SetBitrate", bitRate);
+	}
+
+	public void SetVideoSize(int width, int height)
+	{
+		if (IsAndroidPlatform())
+			screenRecorder?.Call("SetVideoSize", width, height);
+	}
+
+	public void SetVideoEncoder(int encoder)
+	{
+		if (IsAndroidPlatform())
+			screenRecorder?.Call("SetVideoEncoder", encoder);
+	}
+
+	public void PreviewVideo(string videoPath)
+	{
+		if (IsAndroidPlatform() && (videoPath != null && File.Exists(videoPath)))
+        {
+			screenRecorder?.Call("PreviewVideo", videoPath);
+			return;
+		}
+
+		if (IsIosPlatform())
+        {
+			StartCoroutine(IosPreview((isSuccess) => {
+				
+			}));
+		}
+	}
+
+	// Wait a bit for preparing the preview
+	private IEnumerator IosPreview (System.Action<bool> callback)
+	{
+		yield return new WaitForSeconds(1.0f);
+		bool isSuccess = _iosRecorder.ShowPreview();
+		callback(isSuccess);
+
+	}
+
+	public void ShareVideo(string filePath,string message, string title)
+	{
+		if (IsAndroidPlatform() &&  (filePath != null && File.Exists(filePath)))
+			screenRecorder?.Call("ShareVideo", filePath,message,title,_fileProvider);
+	}
+
+	public enum VideoEncoder
+	{
+		DEFAULT = 0, H263 = 1, H264 = 2, MPEG_4_SP = 3, VP8 = 4, HEVC = 5
+	}
+
+
+	//CallBack From Android library
+	public void OnRecordPermissionGranted(string status)
+	{
+		if (status == "True")
+        {
+			Debug.Log("Unity>> Record Permission Granted  ");
+        }
+        else
+        {
+			Debug.Log("Unity>> Record Permission Not Granted ");
+		}
+	}
+
+	public void OnRecordStartStatus(string status)
+	{
+		if (status == "True")
+		{
+			Debug.Log("Unity>> Record Started  ");
+		}
+		else
+		{
+			Debug.Log("Unity>> Record Failed ");
+		}
+	}
+
+
+	// Only Work in IOS
+	public bool IsRecordingAvailable()
+	{
+		if (IsIosPlatform())
+		{
+			return _iosRecorder.IsRecorderAvailable();
+		}
+
+		return true;
+	}
+
+	// Only Work in IOS
+	private void OnIosRecordStatus(bool isSuccess)
+    {
+		if (isSuccess)
+		{
+			Debug.Log("Unity>> Ios Record Started  ");
+		}
+		else
+		{
+			Debug.Log("Unity>> Ios Record Failed  ");
+		}
+		
+	}
+
+	public bool IsAndroidPlatform()
+	{
+		bool result = false;
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+		result = true;
+#endif
+
+		return result;
+	}
+
+	public bool IsIosPlatform()
+	{
+		bool result = false;
+
+#if UNITY_IPHONE && !UNITY_EDITOR
+		result = true;
+#endif
+
+		return result;
+	}
+
+
+}
